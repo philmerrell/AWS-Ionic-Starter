7d470deaa60cb8451351f6f7118020296f81125f
philmerrell <philmerrell@boisestate.edu>
2020-03-07 11:48:01 -0700

    chore: update login flow

--- client/src/app/home/home.page.ts
+++ client/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -6,9 +6,14 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  isAuthenticated = false;
 
   constructor(private authService: AuthService) {}
+
+  async ngOnInit() {
+    this.isAuthenticated = await this.authService.isAuthenticated();
+  }
 
   login() {
     this.authService.login();
