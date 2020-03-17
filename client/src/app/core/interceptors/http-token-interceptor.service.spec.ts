dddb2a295a4e6d9e07250445af54ad55e852f1a9
philmerrell <philmerrell@boisestate.edu>
2020-03-16 22:55:55 -0600

    test: fix test

--- client/src/app/core/interceptors/http-token-interceptor.service.spec.ts
+++ client/src/app/core/interceptors/http-token-interceptor.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpTokenInterceptor } from './http-token-interceptor.service';
 
-import { HttpTokenInterceptorService } from './http-token-interceptor.service';
 
 describe('HttpTokenInterceptorService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: HttpTokenInterceptorService = TestBed.get(HttpTokenInterceptorService);
+    const service: HttpTokenInterceptor = TestBed.get(HttpTokenInterceptor);
     expect(service).toBeTruthy();
   });
 });
